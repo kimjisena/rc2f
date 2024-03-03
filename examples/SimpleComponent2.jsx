4d7aab2ca8fd5917d82b687d933326b355dadcee
Kim Jisena <kimjisena@gmail.com>
2024-03-03 15:54:50 +0300

    feat: parse state variables, add `prettier` for formatting outing

--- examples/SimpleComponent2.jsx
+++ examples/SimpleComponent2.jsx
@@ -1,6 +1,39 @@
-import React, { Component } from "react";
+import React, { creatRef, Component } from "react";
+import Icon from "icon-lib";
 
 class SimpleComponent2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "john",
+      age: 42,
+    };
+  }
+
+  componentDidMount() {
+    console.log("component mounted!");
+  }
+  componentDidUnmount() {
+    console.log("component unmounted!");
+  }
+  componentDidUpdate() {
+    console.log("component updated!");
+  }
+  componentDidCatch(error, info) {
+    console.log("the error boundary");
+  }
+  componentWillMount() {
+    console.log("component about to mount!");
+  }
+  componentWillReceiveProps(nextProps) {
+    console.log("component about to receive props!");
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("component about to update!");
+  }
+  componentWillUnmount() {
+    console.log("component about to unmount!");
+  }
   render() {
     return <p>Hello from SimpleComponent2!</p>;
   }
--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import BabelParser from "@babel/parser";
-import { handleClassDeclaration, handleOtherDeclarations } from "./src/lib";
+import {
+  generateFunctionComponent,
+  handleClassDeclaration,
+  handleOtherDeclarations,
+} from "./src/lib";
 
 // 1. read input file
 
 const inputFilePath =
-  "/Users/0xrafikii/Documents/Playground/Scripts/rc2f/examples/SimpleComponent.jsx";
+  "/Users/0xrafikii/Documents/Playground/Scripts/rc2f/examples/SimpleComponent2.jsx";
 const inputFile = Bun.file(inputFilePath);
 console.log("read", inputFile.size, "bytes of file type", inputFile.type);
 
@@ -14,14 +18,13 @@ const inputFileContent = await inputFile.text();
 // 2. parse content of input file
 
 const ast = BabelParser.parse(inputFileContent, {
-  sourceFilename: "SimpleComponent.jsx",
+  sourceFilename: "SimpleComponent2.jsx",
   sourceType: "module",
   plugins: ["jsx"],
 }).program.body;
 
 // console.log(ast);
 ast.forEach((stmt, idx, _) => {
-  console.log(stmt.type);
   if (
     stmt.type === "ImportDeclaration" ||
     stmt.type === "ExportNamedDeclaration" ||
@@ -35,8 +38,12 @@ ast.forEach((stmt, idx, _) => {
     handleOtherDeclarations(stmt.type, declaration);
   }
   if (stmt.type === "ClassDeclaration") {
-    handleClassDeclaration(stmt);
+    console.log(stmt.type);
+    handleClassDeclaration(stmt, inputFileContent);
   }
 });
+
 // 3. transform class to function
+generateFunctionComponent();
+
 // 4. write output file
